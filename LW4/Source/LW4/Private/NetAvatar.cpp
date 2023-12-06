061d9c3223babe647ef132a48ab5d0f7d71a797c
feuskem <118988514+feuskem@users.noreply.github.com>
2023-12-06 21:20:21 +0300

    LW4 Unreal Engine Version 5.2.1

---
+++ LW4/Source/LW4/Private/NetAvatar.cpp
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NetAvatar.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ANetAvatar::ANetAvatar():
+	Speed(300.0f),
+	WalkSpeed(300.0f),
+	RunSpeed(600.0f)
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+}
+
+
+
+void ANetAvatar::ChangingSpeed_Implementation(float Amount)
+{
+	Speed = Amount;
+	if (HasAuthority())
+	{
+		OnRep_PlayerSpeedChanged();
+	}
+}
+
+void ANetAvatar::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const {
+
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ANetAvatar, Speed);
+}
+
+void ANetAvatar::OnRep_PlayerSpeedChanged()
+{
+	GetCharacterMovement()->MaxWalkSpeed = Speed;
+}
+
+void ANetAvatar::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Camera->bUsePawnControlRotation = false;
+	SpringArm->bUsePawnControlRotation = true;
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->MaxWalkSpeed = Speed;
+
+}
+
+void ANetAvatar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Turn", this, &ACharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("Lookup", this, &ACharacter::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ANetAvatar::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ANetAvatar::MoveRight);
+
+	PlayerInputComponent->BindAction("Running", EInputEvent::IE_Pressed, this, &ANetAvatar::RunPressed);
+	PlayerInputComponent->BindAction("Running", EInputEvent::IE_Released, this, &ANetAvatar::RunReleased);
+}
+
+void ANetAvatar::MoveForward(float Scale)
+{
+	FRotator Rotation = GetController()->GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+	FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ForwardDirection, Scale);
+}
+
+void ANetAvatar::MoveRight(float Scale)
+{
+	FRotator Rotation = GetController()->GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+	FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(ForwardDirection, Scale);
+}
+
+void ANetAvatar::RunPressed()
+{
+	Speed = RunSpeed;
+	ChangingSpeed(Speed);
+}
+
+void ANetAvatar::RunReleased()
+{
+	Speed = WalkSpeed;
+	ChangingSpeed(Speed);
+}
